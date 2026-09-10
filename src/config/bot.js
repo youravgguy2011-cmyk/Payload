713471a4945f5e782e1396efc0256a896187ee1d
Circuit <youravgguy2011@gmail.com>
2026-09-09 20:41:37 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "I See You.", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "Payload.", // required by Discord API, not shown in the client
+        state: "I See You",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
